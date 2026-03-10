8b6290042a5e20e26714c4206b5dbfb0570adb6e
D-a1-2 <2138625429@qq.com>
2026-03-10 22:27:36 +0800

    Add Helloworld.java to output greeting message

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+/**
+ * Helloworld.java
+ * 功能：输出一行问候语，验证 Java 环境配置正常。
+ */
+public class Helloworld {
+
+    /**
+     * 主方法：程序入口
+     * @param args 命令行参数（本程序不使用）
+     */
+    public static void main(String[] args) {
+        // 向控制台输出 Hello, World!
+        System.out.println("Hello, World!");
+    }
+}
